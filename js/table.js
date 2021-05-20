d4c85fa36e27f6ed2a1a79f08d97fd79b49c0c35
Brandon Toushan <14bwt1@queensu.ca>
2021-05-20 02:14:36 -0400

    Update table.js
    cleanup

--- js/table.js
+++ js/table.js
@@ -1,38 +1,44 @@
 //pulling .csv file from github repo and parsing
-    Papa.parse(url, {
+Papa.parse(url, 
+{
         download: true,
         complete: function(result) {
-            if (result.data && result.data.length > 0) {
+            if (result.data && result.data.length > 0) 
+            {
                 htmlTableGenerator(result.data)
             }
         }
-    });
+});
 
 //generating html datatable
-function htmlTableGenerator(content) {
+function htmlTableGenerator(content)
+{
     let csv_preview = document.getElementById('data-table');
-
     let html = '<table id="example" class="table table-condensed table-hover table-striped" style="width:100%">';
-
-    if (content.length == 0 || typeof(content[0]) === 'undefined') {
+    if (content.length == 0 || typeof(content[0]) === 'undefined') 
+    {
         return null
-    } else {
+    }
+    else 
+    {
         const header = content[0];
         const data = content.slice(1);
-
         html += '<thead>';
         html += '<tr>';
-        header.forEach(function(colData) {
+        header.forEach(function(colData) 
+        {
             html += '<th>' + colData + '</th>';
         });
         html += '</tr>';
         html += '</thead>';
         html += '<tbody>';
-
-        data.forEach(function(row) {
-            if (header.length === row.length) {
+        data.forEach(function(row) 
+        {
+            if (header.length === row.length)
+            {
                 html += '<tr>';
-                row.forEach(function(colData) {
+                row.forEach(function(colData) 
+                {
                     html += '<td>' + colData + '</td>';
                 });
                 html += '</tr>';
