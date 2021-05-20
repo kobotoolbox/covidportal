1e8a6dc23cd10f7b2a33b62473c09eff2ea3c965
Brandon Toushan <14bwt1@queensu.ca>
2021-05-20 02:10:54 -0400

    Update map.js
    cleanup

--- js/map.js
+++ js/map.js
@@ -1,62 +1,77 @@
-var map = new Datamap({
-  element: document.getElementById("map"),
-  projection: 'mercator',
-  responsive: true,
-  fills:
+var map = new Datamap(
   {
-    defaultFill: '#C8DCE7',
-    EXTRA_HIGH: '#2B5487',
-    HIGH: '#008DC9',
-    MEDIUM: '#5CC6F2',
-    LOW: '#90DEFF',
-  },
-  data: {
-    COD: {
-      fillKey: 'HIGH',
-      num_surveys: 1
+    element: document.getElementById("map"),
+    projection: 'mercator',
+    responsive: true,
+    fills:
+    {
+      defaultFill: '#C8DCE7',
+      EXTRA_HIGH: '#2B5487',
+      HIGH: '#008DC9',
+      MEDIUM: '#5CC6F2',
+      LOW: '#90DEFF',
     },
-    USA: {
-      fillKey: 'MEDIUM',
-      num_surveys: 1
+    data: 
+    {
+      COD:
+      {
+        fillKey: 'HIGH',
+        num_surveys: 1
+      },
+      USA:
+      {
+        fillKey: 'MEDIUM',
+        num_surveys: 1
+      },
+      FRA: 
+      {
+        fillKey: 'LOW',
+        num_surveys: 1
+      },
+      LSO: 
+      {
+        fillKey: 'EXTRA_HIGH',
+        num_surveys: 1
+      },
+      GTM: 
+      {
+        fillKey: 'EXTRA_HIGH',
+        num_surveys: 1
+      },
+      UGA: 
+      {
+        fillKey: 'EXTRA_HIGH',
+        num_surveys: 1
+      }
     },
-    FRA: {
-      fillKey: 'LOW',
-      num_surveys: 1
+    geographyConfig: 
+    {
+      popupTemplate: function(geography, data) 
+      {
+        return ['<div class = "hoverinfo"><strong>',
+                'Survey Results from ' + geography.properties.name,
+                ': ' + data.num_surveys,
+                '</strong></div>'].join('')
+      }
     },
-    LSO: {
-      fillKey: 'EXTRA_HIGH',
-      num_surveys: 1
-    },
-    GTM: {
-      fillKey: 'EXTRA_HIGH',
-      num_surveys: 1
-    },
-    UGA: {
-      fillKey: 'EXTRA_HIGH',
-      num_surveys: 1
-    }
-  },
-  geographyConfig: {
-    popupTemplate: function(geography, data) {
-      return ['<div class = "hoverinfo"><strong>',
-        'Survey Results from ' + geography.properties.name,
-        ': ' + data.num_surveys,
-        '</strong></div>'].join('')
-    }
-  },
-  done: function(datamap) {
-
-//on-click function
-    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-      window.open(geography.properties.name + "_landing_page.html");});
-
-//zoom function
-    datamap.svg.call(d3.behavior.zoom().on("zoom", redraw));
-    function redraw() {
-      datamap.svg.selectAll("g").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    done: function(datamap) 
+    {
+      //on-click function
+      datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) 
+      {
+        window.open(geography.properties.name + "_landing_page.html");
+      });
+      
+      //zoom function
+      datamap.svg.call(d3.behavior.zoom().on("zoom", redraw));
+      function redraw() 
+      {
+        datamap.svg.selectAll("g").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
       }
     }
-});
-window.addEventListener("resize", function() {
+  });
+
+window.addEventListener("resize", function() 
+{
   map.resize();
 });
