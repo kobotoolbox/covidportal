b7245da50faf8c6c20beeb55592fb9ece8a3ec19
Brandon Toushan <14bwt1@queensu.ca>
2021-05-22 20:34:16 -0400

    Update auto.js

--- js/auto.js
+++ js/auto.js
@@ -1,18 +1,19 @@
+//function autoDraw takes in a dataset (.csv) and an aggregation level ('Total','Aggregated') and dynamically produces a chart for each unique (Group,Question) pair present in the dataset
 function autoDraw(csv,agg) 
 {
-   //parsing csv
+   //parsing .csv with Papa Parse
    var results = Papa.parse(csv,
    {
       download: true,
       complete: function(results) 
       {
-         //extracting groups + questions from .csv
+         //extracting all groups + questions from dataset
          var array = results.data.slice(1,-1)
          var groups = array.map(function(value,index) { return value[0]; });
          var questions = array.map(function(value,index) { return value[1]; });
          pairs = [], i = 0;
          
-         //extracting all unique group;question pairs
+         //extracting all unique (Group,Question) pairs
          while ( groups[++i] ) 
          {
             pairs.push( [ groups[i], questions[i] ] );}
@@ -28,11 +29,11 @@ function autoDraw(csv,agg)
                output.push(item);
             }};
          
-         //formatting output
+         //mapping all unique (Group,Question) pairs to sets
          var groups_set = output.map(function(value,index) { return value[0]; });
          var questions_set = output.map(function(value,index) { return value[1]; });
          
-         //passing all unique group;question pairs to charting func
+         //passing all unique (Group,Question) pairs to drawGraphic()
          groups_set.forEach((group, index) => 
          {
             const question = questions_set[index];
